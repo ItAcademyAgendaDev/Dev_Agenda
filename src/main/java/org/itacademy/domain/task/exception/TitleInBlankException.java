2005dac279e3b79d83fe637c689b1eca73ab4b1e
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-03-07 00:28:43 +0100

    feat: add interactive CLI menu for task management

---
+++ src/main/java/org/itacademy/domain/task/exception/TitleInBlankException.java
@@ -0,0 +1,7 @@
+package org.itacademy.domain.task.exception;
+
+public class TitleInBlankException extends RuntimeException {
+    public TitleInBlankException(String message) {
+        super(message);
+    }
+}
