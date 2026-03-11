380abd073c43b261a5d09b77859fa435625a06c8
Javier Castillo <javiercame98@gmail.com>
2026-03-11 10:52:57 +0100

    Feature/create event (#14)
    * feat: creating event structure and create event functionality
    
    * feat: creating event

--- src/main/java/org/itacademy/repository/config/DatabaseConnectionFactory.java
+++ src/main/java/org/itacademy/repository/config/DatabaseConnectionFactory.java
@@ -4,6 +4,8 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 
 public class DatabaseConnectionFactory  {
+    private DatabaseConnectionFactory(){}
+
     public static Connection createConnection(String url, String user, String password) {
         try {
             return DriverManager.getConnection(url, user, password);
